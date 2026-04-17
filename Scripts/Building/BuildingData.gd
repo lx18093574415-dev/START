ade83becffa79b23a5971e00b296d86b541b9660
lx18093574415-dev <lx18093574415@gmail.com>
2026-04-18 01:21:19 +0800

    Expand building data schema

--- Scripts/Building/BuildingData.gd
+++ Scripts/Building/BuildingData.gd
@@ -2,8 +2,12 @@
 class_name BuildingData
 extends Resource
 
-@export var name: String
+@export var building_id: StringName
+@export var display_name: String
+@export var category: StringName
+@export var icon: Texture2D
 @export var scene: PackedScene
 @export var preview: PackedScene
 @export var size: Vector2
-@export var cost: int
+@export var cost_wood: int
+@export var cost_stone: int
