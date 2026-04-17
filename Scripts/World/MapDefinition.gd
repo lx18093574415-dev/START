2e18b94b1f56caa321f240813b7a30246b314d21
lx18093574415-dev <lx18093574415@gmail.com>
2026-04-18 03:02:40 +0800

    Add map definition resource

---
+++ Scripts/World/MapDefinition.gd
@@ -0,0 +1,9 @@
+class_name MapDefinition
+extends Resource
+
+@export var map_id: StringName = &"starter_basin"
+@export var world_size := Vector2i(200, 200)
+@export var chunk_size := 10
+@export var map_seed := 42042
+@export var visible_chunk_radius := 1
+@export var starter_clear_radius := 12.0
